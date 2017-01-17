160b47648bd74f44445a53e474b5bbb1ea14dda9
Zack Raycraft <raycraftzack@aol.com>
2017-01-17 14:31:00 -0500

    Second commit.

--- HousesExample/House.cs
+++ HousesExample/House.cs
@@ -8,5 +8,21 @@ namespace HousesExample
 {
     class House
     {
+        //Add four fields to your class
+        private string color;
+        private int rooms;
+        private int size;
+        private int doors;
+
+        //Add methods to your class
+        public void Paint(string color)
+        {
+            this.color = color;
+        }
+
+        public string CurrentColor()
+        {
+            return color;
+        }
     }
 }
